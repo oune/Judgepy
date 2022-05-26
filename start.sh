2716a8e746f8330e882343d99a33cb15e047fb90
Junjae Lee <ejj612o@gmail.com>
2022-05-26 21:17:55 +0900

    개선

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 rootPath=$(pwd)
+mkdir -p out
 
-while read line || [ -n "$line" ] ; do
+while read line; do
   cd "$rootPath"
 
   mainPath="${line%.java}"
   dirPath="${mainPath%/?*}"
   className="${mainPath##?*/}"
+  outName=`echo $mainPath | tr -d '/' | tr -d '.'`
 
   echo "target: $dirPath"
   echo "className: $className"
+  echo "outPath: $outPath"
 
   cd "$dirPath"
   echo "compiling..."
-  javac *.java
+  javac *.java && echo "compile success" || echo "compile failed"
+  java $className < "$rootPath/testcase/1.txt" >> "$rootPath/out/$outName.txt" && echo "java run success" || echo "java run fail"
 
   echo ""
-done
+done 
