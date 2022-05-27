0a1ce0aec6c2eafaf3f4773ecca16fa71170cc23
Junjae Lee <ejj612o@gmail.com>
2022-05-27 16:19:52 +0900

    /testcase/ 하위 폴데 txt 파일 기준으로 반복 하도록 변경

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 rootPath=$(pwd)
 mkdir -p out
 
-while read line; do
+while read line;
+do
   cd "$rootPath"
 
   mainPath="${line%.java}"
@@ -13,12 +14,22 @@ while read line; do
 
   echo "target: $dirPath"
   echo "className: $className"
-  echo "outPath: $outPath"
 
   cd "$dirPath"
   echo "compiling..."
   javac *.java && echo "compile success" || echo "compile failed"
   java $className < "$rootPath/testcase/1.txt" >> "$rootPath/out/$outName.txt" && echo "java run success" || echo "java run fail"
+
+  for codeDir in "$rootPath"/testcase/*/
+  do
+	 subjectDir=${codeDir%/}
+	 subjectName=${subjectDir##?*/}
+	 
+	 for testcase in "$codeDir"*
+	 do
+		 echo $testcase
+	 done
+  done
 
   echo ""
 done 
